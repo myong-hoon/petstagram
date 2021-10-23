0ae22744c5eae40c3df9653dc0e7c781b815c4dd
myong-hoon <92171034+myong-hoon@users.noreply.github.com>
2021-10-23 20:18:33 +0900

    sys 오류 수정

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, jsonify, request
 import sys
+sys.path.append('/Users/yongyong/Desktop/petstagram')
 import security
 from pymongo import MongoClient
 import boto3
 from botocore.exceptions import NoCredentialsError
-sys.path('/Users/yongyong/Desktop/petstagram')
+
 app = Flask(__name__)
 ACCESS_KEY = security.ACCESS_KEY
 SECRET_KEY = security.SECRET_KEY
